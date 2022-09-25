8aa721e62d1e11d308c0553e534ca4fcd8b4099f
Erick Hualla <ehuallap@unsa.edu.pe>
2022-09-25 18:24:40 -0500

    Problem p11799 and template updated

--- ProblemSet01/p11799/main.cpp
+++ ProblemSet01/p11799/main.cpp
@@ -29,6 +29,18 @@ typedef vector<pii> vpii;
 
 void solution() {
     //ofstream out("out.txt");
+    int numCases; scanf("%i", &numCases);
+    loop(i, 0, numCases) {
+        int numMons;
+        int highestV = -1;
+        int actualV;
+        scanf("%i", &numMons);
+        loop(j, 0, numMons) {
+            scanf("%i", &actualV);
+            actualV > highestV ? highestV = actualV : highestV = highestV;
+        }
+        printf("Case %i: %i\n", i+1, highestV);
+    }
 }
 
 int main() {
