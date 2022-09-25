2b7f7f2406d8c68abc18062ba27080cb58cd8dbb
Erick Hualla <ehuallap@unsa.edu.pe>
2022-09-25 17:25:44 -0500

    Problem p10071 solved

---
+++ ProblemSet01/p10071/main.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <climits>
+#include <queue>
+#include <vector>
+#include <map>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <stack>
+#include <algorithm>
+#include <cstring>
+
+#define max(a,b) (a<b?b:a)
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define SQ(x) (x)*(x)
+#define loop(i,v,n) for(int i=v;i<n;i++)
+#define print(i,V) for(auto &i : V) cout << i << endl;
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+void solution() {
+    int finalV, timeT;
+    while(cin >> finalV >> timeT)
+        cout << 2*(finalV*timeT) << endl;
+}
+
+int main() {
+    solution();
+    return 0;
+}
