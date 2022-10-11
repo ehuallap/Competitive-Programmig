906725388fbf48155a81e2352419929b3c2bf1dd
Erick Hualla <ehuallap@unsa.edu.pe>
2022-10-11 00:08:25 -0500

    Template updated

--- Lab/union-find/main.cpp
+++ Lab/union-find/main.cpp
@@ -28,13 +28,37 @@ typedef pair<int,int> pii;
 typedef vector<pii> vpii;
 typedef vector<int> vb;
 
-void solution() {
-    //ofstream out("out.txt");
+#define MAXN 1000010
+
+vector<int> lst[MAXN];
+int parent[MAXN];
+
+void make_set(int v) {
+    lst[v] = vector<int>(1, v);
+    parent[v] = v;
 }
 
+int find_set(int v) {
+    return parent[v];
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (lst[a].size() < lst[b].size())
+            swap(a, b);
+        while (!lst[b].empty()) {
+            int v = lst[b].back();
+            lst[b].pop_back();
+            parent[v] = a;
+            lst[a].push_back (v);
+        }
+    }
+}
+
+// Driver progxrm to test above functions
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    solution();
-    return 0;
+
+	return 0;
 }
